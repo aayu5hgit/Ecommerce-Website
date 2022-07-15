6dde1d54172f4ae0938049189b4f68a66abe8bac
AAYUSH TALREJA <amtalreja02@gmail.com>
2022-07-15 18:55:02 +0530

    Second Commit

--- config/database.js
+++ config/database.js
@@ -1,11 +1,27 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
+  defaultConnection: "default",
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "testdb"),
+      user: env("DATABASE_USER", "postgres"),
+      password: env("DATABASE_PASSWORD", "aayush@02"),
+      schema: env("DATABASE_SCHEMA", "public"),
     },
-    useNullAsDefault: true,
-  },
+  }
 });
+
+
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
